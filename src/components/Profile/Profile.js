7f4a149cea00c4d1dc140c9cfeab2e01613f3528
Kevin-Clement <kevin-clement@live.fr>
2022-02-06 14:21:20 +0100

    Securing the private route

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -2,23 +2,38 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import NavBar from '../NavBar/NavBar';
 import { getUser } from '../../services/userApiService';
+import { useNavigate } from 'react-router-dom';
+import Button from '@mui/material/Button';
 
 const Profile = () => {
 
     const [user, setUser] = useState();
+    const [userData, setUserData] = useState(localStorage.getItem('connectedUser') || '');
+    const navigate = useNavigate();
+
+    const connectedUser = JSON.parse(userData);
 
     useEffect(() => {
-        getUser().then((res) => {
+        getUser(connectedUser.id).then((res) => {
             res.status === 200 && setUser(res.data);
         }).catch((err) => {
             console.log(err);
         })
     }, [])
 
+    const handleLogout = () => {
+        localStorage.clear();
+        navigate("/login")
+    }
+
     return (
         <div>
             <h1>Bienvenue sur votre profil</h1>
-            {user?.email && <p>Adresse email : {user?.email}</p> }
+            {user?.email && <p>Adresse email : {user?.email}</p>}
+            <p>{user?.firstName} {user?.lastName}</p>
+            <br />
+            <br />
+            <Button color="error" variant="contained" onClick={() => handleLogout()}>Déconnexion</Button>
             <NavBar/>
         </div>
     );
