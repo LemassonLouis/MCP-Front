1e6e93183234e0b587e57c880e11f6ec61759bc4
Kevin-Clement <kevin-clement@live.fr>
2022-02-22 21:47:14 +0100

    Implementation public route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import React, { useState, useEffect } from 'react';
-import UserContext from './Contexts/UserContext';
+import UserContext from './contexts/UserContext';
 import "./App.css";
 import Home from "./components/Home/Home";
 import Profile from "./components/Profile/Profile";
@@ -15,9 +15,9 @@ import Login from "./components/Login/Login";
 import { ThemeProvider } from '@mui/material/styles';
 import theme from "./Utils/Theme/Colors"
 import Registration from "./components/Registration/Registration";
-import PrivateRoute from "./Routes/PrivateRoute";
+import PrivateRoute from "./routes/PrivateRoute";
+import PublicRoute from "./routes/PublicRoute";
 import Category from "./components/category/Category";
-
 
 function App() {
 
@@ -25,20 +25,26 @@ function App() {
 
   useEffect(()=>{
     const user = JSON.parse(localStorage.getItem("connectedUser"))
-    if(user){
-      setCurrentUser(user)
-    }
+      user && setCurrentUser(user)
   },[])
 
   return (
     <LocalizationProvider dateAdapter={DateFnsAdapter}>
       <ThemeProvider theme={theme}>
         <UserContext.Provider value={{currentUser,setCurrentUser}} >
-          <div className="App">
+          <div className="App"> 
             <Routes>
               <Route path="*" element={<NotFound/>}/>
-              <Route path="/login" element={<Login/>}/>
-              <Route path="/registration" element={<Registration/>}/>
+              <Route path="/login" element={
+                <PublicRoute>
+                  <Login/>
+                </PublicRoute>
+              }/>
+              <Route path="/registration" element={
+                <PublicRoute>
+                  <Registration/>
+                </PublicRoute>
+                }/>
               <Route path="/" element={
                 <PrivateRoute>
                   <Home/>
