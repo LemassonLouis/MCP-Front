f1521204414a8359f069e4e6c9ee7df7bd71f4d9
vincentgenouel <zincent@hotmail.fr>
2022-02-23 09:42:51 +0100

    Change from antd to mui

--- src/components/category/Category.js
+++ src/components/category/Category.js
@@ -1,74 +1,84 @@
-// import CategoryForm from "./CategoryForm";
-// // import { DeleteOutlined } from "@ant-design/icons/lib/icons";
-// // import { Card, Spin } from "antd";
-// import { useCallback, useEffect, useState } from "react";
-// import { deleteCategory, getAllCategories } from "../../Api/CategoryService";
-// import Nav from "../NavBar/Nav";
+import CategoryForm from "./CategoryForm";
+import { useCallback, useEffect, useState } from "react";
+import NavBar from "../NavBar/NavBar";
+import {
+  deleteCategory,
+  getAllCategories,
+} from "../../services/CategoryService";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CircularProgress,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Box from "@mui/material/Box";
 
-// const { Meta } = Card;
+const Category = ({ onClick }) => {
+  const [categoriesState, setCategoriesState] = useState([]);
+  const [categoriesLoading, setCategoriesLoading] = useState(false);
 
-// const Category = ({ onFinish }) => {
-//   const [categoriesState, setCategoriesState] = useState([]);
-//   const [categoriesLoading, setCategoriesLoading] = useState(false);
+  const initCategories = useCallback(async () => {
+    try {
+      setCategoriesLoading(true);
+      const categories = await getAllCategories();
+      if (categories.status === 200) {
+        setCategoriesLoading(false);
+        setCategoriesState(categories.data);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
-//   const initCategories = useCallback(async () => {
-//     try {
-//       setCategoriesLoading(true);
-//       const categories = await getAllCategories();
-//       if (categories.status === 200) {
-//         setCategoriesLoading(false);
-//         setCategoriesState(categories.data);
-//       }
-//     } catch (err) {
-//       console.log(err);
-//     }
-//   }, []);
+  const deleteCategoryById = useCallback(
+    async (id) => {
+      console.log(id);
+      await deleteCategory(id);
+      initCategories();
+    },
+    [initCategories]
+  );
 
-//   const deleteCategoryById = useCallback(
-//     async (id) => {
-//       console.log(id);
-//       await deleteCategory(id);
-//       initCategories();
-//     },
-//     [initCategories]
-//   );
+  useEffect(() => {
+    if (onClick) {
+      initCategories();
+    }
+    initCategories();
+  }, [initCategories, onClick]);
 
-//   useEffect(() => {
-//     if (onFinish) {
-//       initCategories();
-//     }
-//     initCategories();
-//   }, [initCategories, onFinish]);
+  return (
+    <>
+      <CategoryForm onClick={initCategories} />
+      <div className="card">
+        {categoriesLoading && <CircularProgress />}
+        {categoriesState.map((e) => {
+          return (
+            <div className="uniqueCard" key={e.id}>
+              <Card sx={{ minWidth: 275 }}>
+                <CardContent>
+                  <Typography
+                    component="div"
+                    variant="h4"
+                    color="text.secondary"
+                  >
+                    {e.name}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <IconButton variant="center">
+                    <DeleteIcon onClick={() => deleteCategoryById(e.id)} />
+                  </IconButton>
+                </CardActions>
+              </Card>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
 
-//   return (
-//     <>
-//       <Spin spinning={categoriesLoading}>
-//         <div className="card">
-//           {categoriesState.map((e) => {
-//             return (
-//               <div className="uniqueCard" key={e.id}>
-//                 <Card
-//                   loading={categoriesLoading}
-//                   // key={e.id}
-//                   style={{ width: 200, marginTop: 16 }}
-//                   actions={[
-//                     <DeleteOutlined
-//                       key="deleted"
-//                       onClick={() => deleteCategoryById(e.id)}
-//                     />,
-//                   ]}
-//                 >
-//                   <Meta title={e.name} />
-//                 </Card>
-//               </div>
-//             );
-//           })}
-//         </div>
-//       </Spin>
-//       <CategoryForm onFinish={initCategories} />
-//       <Nav />
-//     </>
-//   );
-// };
-
-// export default Category;
+export default Category;
