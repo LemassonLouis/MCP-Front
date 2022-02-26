96e1ea3d0997e8ba56d78ea119c61f6d67d032dc
LEMASSON Louis <galou2608@gmail.com>
2022-02-26 13:58:19 +0100

    Fixing import path of Routes and Contexts in many files

--- src/components/Common/Header/Header.js
+++ src/components/Common/Header/Header.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import './Header.css';
 import { useContext } from 'react';
-import UserContext from '../../../contexts/UserContext';
-import {useNavigate} from 'react-router-dom';
+import UserContext from '../../../Contexts/UserContext';
+import { useNavigate } from 'react-router-dom';
 
 
 const Header = () => {
 
     const navigate = useNavigate();
-    const {currentUser} = useContext(UserContext);
+    const { currentUser } = useContext(UserContext);
 
     return (
         <div className='webapp-header webapp-header_desktop'>
-            <h1 className='webapp-header_logo' onClick={()=>navigate('/')}>LOGO</h1>
+            <h1 className='webapp-header_logo' onClick={() => navigate('/')}>LOGO</h1>
             {currentUser?.roles[0] && <p>Rôle : {currentUser?.roles[0] === 'ROLE_ADMIN' ? 'Administrateur' : 'Lecteur'}</p>}
         </div>
     );
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { useState, useEffect, useContext } from 'react';
-import UserContext from '../../contexts/UserContext';
-import {useNavigate} from 'react-router-dom';
-import {TextField, Alert} from '@mui/material';
+import UserContext from '../../Contexts/UserContext';
+import { useNavigate } from 'react-router-dom';
+import { TextField, Alert } from '@mui/material';
 import { login } from '../../services/userApiService';
 import LoadingButton from '@mui/lab/LoadingButton';
 
 const Login = () => {
 
-    const {currentUser,setCurrentUser} = useContext(UserContext);
+    const { currentUser, setCurrentUser } = useContext(UserContext);
     const navigate = useNavigate();
     const [inputs, setInputs] = useState({});
     const [validInput, setValidInput] = useState(false);
@@ -20,13 +20,13 @@ const Login = () => {
      */
     useEffect(() => {
         setTimeout(() => {
-            if(alert){
+            if (alert) {
                 setAlert(false)
                 setValidInput(false)
             }
         }, 4000)
     }, [alert]);
-    
+
     /**
      * Recovery of data entered by the user in the inputs
      */
@@ -34,20 +34,20 @@ const Login = () => {
         const target = e.target;
         const value = target.value;
         const name = target.name;
-        setInputs(values => ({...values, [name]: value}));
+        setInputs(values => ({ ...values, [name]: value }));
     }
 
     const signIn = (e) => {
         setLoad(true);
         e.preventDefault();
         let data = {
-            username : inputs.username,
+            username: inputs.username,
             password: inputs.password
         }
         console.log(data);
         login(data).then(
             (res) => {
-                if(res.status === 200) {
+                if (res.status === 200) {
                     localStorage.setItem('token', res.data.token);
                     localStorage.setItem('connectedUser', JSON.stringify(res.data.data));
                     setCurrentUser(res.data.data);
@@ -55,7 +55,7 @@ const Login = () => {
                 }
             }
         ).catch((err) => {
-            if(err.response.status === 401){
+            if (err.response.status === 401) {
                 setValidInput(true)
                 setAlert(true)
                 setLoad(false)
@@ -63,31 +63,31 @@ const Login = () => {
         })
 
     }
-    
+
     return (
         <>
-        <h3>Connexion</h3>
-        <br />
-        <br />
+            <h3>Connexion</h3>
+            <br />
+            <br />
             <form onSubmit={e => signIn(e)}>
-                <TextField 
+                <TextField
                     required
                     error={validInput}
-                    label="Email" 
-                    variant="outlined" 
-                    type="email" 
-                    name="username" 
-                    onInput={handleChange}  />
-                <br/>
+                    label="Email"
+                    variant="outlined"
+                    type="email"
+                    name="username"
+                    onInput={handleChange} />
                 <br />
-                <TextField 
+                <br />
+                <TextField
                     required
                     error={validInput}
-                    label="Mot de passe" 
-                    variant="outlined" 
-                    type="password" 
-                    name="password" 
-                    onInput={handleChange}  />
+                    label="Mot de passe"
+                    variant="outlined"
+                    type="password"
+                    name="password"
+                    onInput={handleChange} />
                 <br />
                 <br />
                 <LoadingButton type="submit" loading={load} color="primary" variant="contained">Connexion</LoadingButton>
