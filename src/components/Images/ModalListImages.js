fd345a919e08bfbaf3e04220963dddbfb348ea1c
LEMASSON Louis <galou2608@gmail.com>
2022-02-27 21:32:15 +0100

    Begin the image button

--- src/components/Images/ModalListImages.js
+++ src/components/Images/ModalListImages.js
@@ -4,27 +4,55 @@ import Modal from '@mui/material/Modal';
 import Fade from '@mui/material/Fade';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import ButtonBase from '@mui/material/ButtonBase';
+import Card from '@mui/material/Card';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
 
 import { ModalStyle } from '../../Utils/ModalStyle';
-// import ModalNewImage from './ModalNewImage';
+import { autocompleteClasses } from '@mui/material';
+import { formatRelative } from 'date-fns';
 
+// import ModalNewImage from './ModalNewImage';
 // import imageApiService, { getAllImages } from '../../services/imageApiService';
 
 
-const style = {
+
+const modal = {
     position: 'absolute',
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
     width: '90%',
-    'max-width': 800,
+    maxWidth: 800,
     bgcolor: 'background.paper',
     border: '1px solid #111',
-    'border-radius': 10,
-    boxShadow: 24,
+    borderRadius: 4,
+    boxShadow: 10,
     pt: 2,
     px: 4,
     pb: 3,
+};
+
+
+
+const card = {
+    position: 'relative',
+    margin: 'auto',
+    marginBottom: 1,
+    width: '70%',
+    maxWidth: 350
+};
+
+
+
+const texte = {
+    position: "absolute",
+    top: "50%",
+    width: "100%",
+    textAlign: "center",
+    backgroundColor: "none",
 };
 
 
@@ -37,7 +65,7 @@ export default function ModalListImage() {
 
     // const [images, setImages] = useState([]);
     // const [searchTerm, setSearchTerm] = useState('');
-    const [openModal, setOpenModal] = useState(false);
+    const [openedModal, setOpenedModal] = useState(false);
 
     // imageApiService.getAllImages().then(res => {
     //     if (res.status == 200) {
@@ -49,28 +77,48 @@ export default function ModalListImage() {
 
     return (
         <div>
-            <Button onClick={(() => { setOpenModal(true) })}>Changer d'image</Button>
-            <Modal
-                // hideBackdrop
-                closeAfterTransition
-                open={openModal}
-            // onClose={() => { setOpenModal(false) }}
+            {/* <Button onClick={() => { setOpenedModal(true) }}>Choisir une image</Button> */}
+            {/* <ButtonBase
+                onClick={() => { setOpenedModal(true) }}
             >
-                <Fade in={openModal}>
-                    <Box sx={style}>
+                <img id="bon" src="127.0.0.1:8000/img/icone image.svg" />
+                <p>Choisir une image</p>
+            </ButtonBase > */}
+            <Card sx={card}>
+                <CardActionArea
+                    onClick={() => { setOpenedModal(true) }}
+                >
+                    <CardMedia
+                        component="img"
+                        // image="127.0.0.1:8000/img/icone image.svg"
+                        image="https://via.placeholder.com/640x480.png/0077ff?text=accusamus"
+                    />
+                    <Typography
+                        component="p"
+                        sx={texte}
+                    >
+                        Choisir une image
+                    </Typography>
+                </CardActionArea>
+            </Card>
+            <Modal
+                closeAfterTransition
+                open={openedModal}
+            // onClose={() => { setOpenedModal(false) }}
+            >
+                <Fade in={openedModal}>
+                    <Box sx={modal}>
                         {/* <ModalNewImage /> */}
-                        <h2 id="parent-modal-title">Liste des images</h2>
+                        <h2 id="parent-modal-title">*Liste des images*</h2>
                         <p id="parent-modal-description">
-                            Recherche<br />
-                            (ajouter une image)<br />
-                            Liste des images<br />
-                            (valider)<br />
+                            (Recherche) (ajouter une image)<br />
+                            (Liste des images)
                         </p>
-                        <Button onClick={() => { setOpenModal(false) }}>VALIDER</Button>
-                        <Button onClick={() => { setOpenModal(false) }}>ANNULER</Button>
+                        <Button onClick={() => { setOpenedModal(false) }}>VALIDER</Button>
+                        <Button onClick={() => { setOpenedModal(false) }}>ANNULER</Button>
                     </Box>
                 </Fade>
             </Modal>
-        </div>
+        </div >
     );
 }
