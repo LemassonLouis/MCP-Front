b27009fc807f978cfdb5a763e527108dc18dc324
LEMASSON Louis <galou2608@gmail.com>
2022-02-27 16:35:19 +0100

    Make the modal working

--- src/components/Images/ModalListImages.js
+++ src/components/Images/ModalListImages.js
@@ -1,13 +1,32 @@
 import React, { useState } from 'react';
 
-import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 
 import { ModalStyle } from '../../Utils/ModalStyle';
 // import ModalNewImage from './ModalNewImage';
 
 // import imageApiService, { getAllImages } from '../../services/imageApiService';
+
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '90%',
+    'max-width': 800,
+    bgcolor: 'background.paper',
+    border: '1px solid #111',
+    'border-radius': 10,
+    boxShadow: 24,
+    pt: 2,
+    px: 4,
+    pb: 3,
+};
+
 
 
 /**
@@ -32,21 +51,25 @@ export default function ModalListImage() {
         <div>
             <Button onClick={(() => { setOpenModal(true) })}>Changer d'image</Button>
             <Modal
-                hideBackdrop
+                // hideBackdrop
+                closeAfterTransition
                 open={openModal}
             // onClose={() => { setOpenModal(false) }}
             >
-                <Box sx={{ ModalStyle, width: 500 }}>
-                    {/* <ModalNewImage /> */}
-                    <h2 id="parent-modal-title">Liste des images</h2>
-                    <p id="parent-modal-description">
-                        Recherche<br />
-                        (ajouter une image)<br />
-                        Liste des images<br />
-                        (valider)<br />
-                    </p>
-                    <Button onClick={() => { setOpenModal(false) }}>VALIDER</Button>
-                </Box>
+                <Fade in={openModal}>
+                    <Box sx={style}>
+                        {/* <ModalNewImage /> */}
+                        <h2 id="parent-modal-title">Liste des images</h2>
+                        <p id="parent-modal-description">
+                            Recherche<br />
+                            (ajouter une image)<br />
+                            Liste des images<br />
+                            (valider)<br />
+                        </p>
+                        <Button onClick={() => { setOpenModal(false) }}>VALIDER</Button>
+                        <Button onClick={() => { setOpenModal(false) }}>ANNULER</Button>
+                    </Box>
+                </Fade>
             </Modal>
         </div>
     );
