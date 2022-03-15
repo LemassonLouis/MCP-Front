87ea5e6e50850e53a99368922d0a6e9f17f825e7
Louis LEMASSON <galou2608@gmail.com>
2022-03-15 13:36:07 +0100

    Display image list

--- src/components/Images/ModalListImages.js
+++ src/components/Images/ModalListImages.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import Modal from '@mui/material/Modal';
 import Fade from '@mui/material/Fade';
@@ -7,12 +7,15 @@ import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardActionArea from '@mui/material/CardActionArea';
 import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
 
 import { ModalStyle } from '../../Utils/ModalStyle';
+import { getAllImages } from '../../services/imageApiService';
+
 
 // import ModalNewImage from './ModalNewImage';
-// import imageApiService, { getAllImages } from '../../services/imageApiService';
 
 
 
@@ -23,6 +26,7 @@ const modalStyle = {
     transform: 'translate(-50%, -50%)',
     width: '90%',
     maxWidth: 800,
+    maxHeight: "90vh",
     bgcolor: 'background.paper',
     border: '1px solid #111',
     borderRadius: 4,
@@ -77,16 +81,31 @@ const imageStyle = {
 
 
 
+const cardContainerStyle = {
+    overflow: "auto",
+};
+
+
+
 /**
  * Component ModalListImage, display a button and show modal by clicking on it.
  * @returns {React.HTML} REACT.HTML
  */
 const ModalListImage = ({ imageID = 0 }) => {
 
-    // const [images, setImages] = useState([]);
     // const [searchTerm, setSearchTerm] = useState('');
-    const [openedModal, setOpenedModal] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [listImages, setListImages] = useState([]);
     const [selectedImage, setSelectedImage] = useState(imageID);
+
+    useEffect(() => {
+        getAllImages()
+            .then(res => {
+                if (res.status === 200) {
+                    setListImages(res.data);
+                }
+            });
+    }, []);
 
     var imageURL;
 
@@ -97,19 +116,11 @@ const ModalListImage = ({ imageID = 0 }) => {
         imageURL = "https://via.placeholder.com/640x500.png/0077ff?text=accusamus";
     }
 
-    // imageApiService.getAllImages().then(res => {
-    //     if (res.status == 200) {
-    //         setImages(res.data);
-    //     }
-    // }).catch(err => {
-    //     console.log(err);
-    // });
-
     return (
         <div>
             <Card sx={cardStyle}>
                 <CardActionArea
-                    onClick={() => { setOpenedModal(true) }}
+                    onClick={() => { setIsModalOpen(true) }}
                 >
                     <CardMedia
                         component="img"
@@ -125,21 +136,52 @@ const ModalListImage = ({ imageID = 0 }) => {
                     </Typography>
                 </CardActionArea>
             </Card>
+
             <Modal
                 closeAfterTransition
-                open={openedModal}
-            // onClose={() => { setOpenedModal(false) }}
+                open={isModalOpen}
+            // onClose={() => { }}
             >
-                <Fade in={openedModal}>
+                <Fade in={isModalOpen}>
                     <Box sx={modalStyle}>
                         {/* <ModalNewImage /> */}
                         <h2 id="parent-modal-title">*Liste des images*</h2>
-                        <p id="parent-modal-description">
-                            (Recherche) (ajouter une image)<br />
-                            (Liste des images)
-                        </p>
-                        <Button onClick={() => { setOpenedModal(false) }}>VALIDER</Button>
-                        <Button onClick={() => { setOpenedModal(false) }}>ANNULER</Button>
+                        <div className='filter'>
+                            <TextField
+                                id="outlined-basic"
+                                label="Recherche"
+                                variant="outlined"
+                                type="text"
+                                name="search"
+                                size='small'
+                            // onChange={((e) => { setSearchTerm(e.target.value) })}
+                            />
+                        </div>
+                        <Button onClick={() => { }}>NOUVELLE IMAGE</Button>
+                        <div sx={cardContainerStyle}>
+                            {
+                                listImages.map(image => {
+                                    return (
+                                        <Card key={image.id} sx={{ width: 250 }}>
+                                            <CardActionArea>
+                                                <CardMedia
+                                                    component="img"
+                                                    height="140"
+                                                    image={image.IMG_uri}
+                                                />
+                                                <CardContent>
+                                                    <Typography gutterBottom variant="h6" component="div">
+                                                        {image.IMG_name}
+                                                    </Typography>
+                                                </CardContent>
+                                            </CardActionArea>
+                                        </Card>
+                                    )
+                                })
+                            }
+                        </div>
+                        <Button onClick={() => { setIsModalOpen(false) }}>VALIDER</Button>
+                        <Button onClick={() => { setIsModalOpen(false) }}>ANNULER</Button>
                     </Box>
                 </Fade>
             </Modal>
