fc8dbc347d4a0ec143e74dfb1c0ede7ada754952
vincentgenouel <zincent@hotmail.fr>
2022-05-08 16:26:02 +0200

    add author

--- src/services/CategoryService.js
+++ src/services/CategoryService.js
@@ -1,3 +1,11 @@
+/**
+ * @author Genouel Vincent
+ * @email genouel.vincent@gmail.com
+ * @create date 2022-05-08 16:21:24
+ * @modify date 2022-05-08 16:21:26
+ * @desc [description]
+ */
+
 import { deleteById, getAll, post } from "./mcpApi";
 
 export const getAllCategories = async (options) =>
