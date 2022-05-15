aa2e6e566f0f793b769c92e9e3305ac16e8deec4
Kevin-Clement <kevin-clement@live.fr>
2022-05-15 02:27:46 +0200

    Adding a padding for the responsiveHeader

--- src/components/Common/Header/ResponsiveHeader.js
+++ src/components/Common/Header/ResponsiveHeader.js
@@ -2,7 +2,7 @@
  * @author Kevin Clément
  * @email kevin-clement@live.fr
  * @create date 2022-04-25 20:23:49
- * @modify date 2022-04-25 20:23:51
+ * @modify date 2022-05-15 02:24:45
  * @desc [description]
  */
 import React from 'react';
@@ -16,7 +16,7 @@ const ResponsiveHeader = ({title}) => {
     const navigate = useNavigate();
 
     return (
-        <div className='webapp-header'>
+        <div className='webapp-header webapp-responsiveHeader'>
             <ArrowBackIcon className='webapp-header_backBtn' onClick={()=>navigate(-1)}/>
             <h2>{title}</h2>
         </div>
